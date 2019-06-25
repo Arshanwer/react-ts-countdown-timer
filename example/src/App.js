a52b0d4f5ef912f88813c8fa2ee63e23b90d5a92
Arshanwer <arshad_91@live.com>
2019-06-25 14:03:06 +0800

    example updaated

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {ReactCountDownTimer} from './dist/index';
+import { ReactCountDownTimer } from './dist/index';
 
 export default class App extends Component {
   constructor(props) {
